1812f639fc7990855c904d32bd0c70c6ad727dae
Maurits van der Schee <maurits@vdschee.nl>
2019-12-02 07:36:59 +0100

    Day01

--- day01/part1.py
+++ day01/part1.py
@@ -1,8 +1,8 @@
 import os
 
 dir = os.path.dirname(__file__)
-sum = 0
 with open(os.path.join(dir,"input")) as f:
+    sum = 0
     for line in f:
         sum+=int(line)//3-2
-print(sum)
+    print(sum)
--- day01/part2.py
+++ day01/part2.py
@@ -1,12 +1,12 @@
 import os
 
 dir = os.path.dirname(__file__)
-sum = 0
 with open(os.path.join(dir,"input")) as f:
+    sum = 0
     for line in f:
         mass = int(line)
         while mass>0:
             mass=mass//3-2
             if mass>0:
                 sum += mass
-print(sum)
+    print(sum)
