b6d879a996b091a92f03239324cd6c9853cabdde
Maurits van der Schee <maurits@vdschee.nl>
2019-12-06 14:03:21 +0100

    Day06

---
+++ day06/part1.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+root = False
+
+dir = os.path.dirname(__file__)
+with open(os.path.join(dir,"input")) as f:
+    children = {}
+    parents = {}
+    for line in f:
+        parent,child = list(line.strip().split(')'))
+        if not parent in children:
+            children[parent] = []
+        children[parent].append(child)
+        parents[child] = parent
+    sum = 0
+    for node in parents:
+        while node in parents:
+            node = parents[node]
+            sum += 1
+    print(sum)
+    
