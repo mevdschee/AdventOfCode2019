9bb4bf6427a9eaea9763c8840b7ed5755b6c41f6
Maurits van der Schee <maurits@vdschee.nl>
2019-12-22 01:11:35 +0100

    Day20

--- day20/part1.py
+++ day20/part1.py
@@ -1,5 +1,6 @@
 import os
 
+
 def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
     result = {}
     for key, pos in positions.items():
@@ -7,7 +8,7 @@ def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
             new_fronts = [(x, y)]
             visited = {(x, y): 0}
             directions = {"north": (0, -1), "south": (0, 1),
-                        "west": (-1, 0), "east": (1, 0)}
+                          "west": (-1, 0), "east": (1, 0)}
             steps = 0
             while len(new_fronts) > 0:
                 steps += 1
@@ -19,8 +20,8 @@ def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
                         nx, ny = x + dx, y + dy
                         if (nx, ny) in field and not (nx, ny) in visited:
                             if field[(nx, ny)] == '.':
-                                if (nx,ny) in portals and portals[(nx,ny)]!=key:
-                                    nkey = portals[(nx,ny)]
+                                if (nx, ny) in portals and portals[(nx, ny)] != key:
+                                    nkey = portals[(nx, ny)]
                                     if not key in result:
                                         result[key] = {}
                                     if not nkey in result:
@@ -31,6 +32,7 @@ def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
                                 new_fronts.append((nx, ny))
     return result
 
+
 dir = os.path.dirname(__file__)
 with open(os.path.join(dir, "input")) as f:
     lines = list(map(list, f.read().rstrip().split("\n")))
@@ -39,11 +41,11 @@ with open(os.path.join(dir, "input")) as f:
     field = {}
     portals = {}
     positions = {}
-    directions = {"north": ((0, -2), (0, -1)), "south": ((0, 1),(0, 2)),
-                  "west": ((-2, 0),(-1, 0)), "east": ((1, 0),(2, 0))}
+    directions = {"north": ((0, -2), (0, -1)), "south": ((0, 1), (0, 2)),
+                  "west": ((-2, 0), (-1, 0)), "east": ((1, 0), (2, 0))}
     for y in range(height):
         for x in range(width):
-            if x<len(lines[y]):
+            if x < len(lines[y]):
                 k = lines[y][x]
                 if k == '.' or k == '#':
                     field[(x, y)] = k
@@ -51,18 +53,18 @@ with open(os.path.join(dir, "input")) as f:
                     for d in directions:
                         dx1, dy1 = directions[d][0]
                         dx2, dy2 = directions[d][1]
-                        c1,c2 = lines[y+dy1][x+dx1],lines[y+dy2][x+dx2]
+                        c1, c2 = lines[y+dy1][x+dx1], lines[y+dy2][x+dx2]
                         if c1.isupper() and c2.isupper():
                             label = c1 + c2
                             portals[(x, y)] = label
                             if not label in positions:
                                 positions[label] = []
-                            positions[label].append((x,y))
+                            positions[label].append((x, y))
 
     distances = calculate_distances(field, portals, positions)
 
     states = {}
-    new_states = {'AA':{
+    new_states = {'AA': {
         "at": "AA",
         "steps": 0,
         "path": ['AA']
@@ -80,7 +82,7 @@ with open(os.path.join(dir, "input")) as f:
                     if not key in state["path"]:
                         new_path = state["path"] + [key]
                         new_steps = state["steps"] + steps+1
-                        if key in new_states and new_states[key]["steps"]<new_steps:
+                        if key in new_states and new_states[key]["steps"] < new_steps:
                             continue
                         new_states[key] = {
                             "at": key,
@@ -88,4 +90,4 @@ with open(os.path.join(dir, "input")) as f:
                             "path": new_path
                         }
 
-    print(min(map(lambda s:s["steps"]-1,results)))
+    print(min(map(lambda s: s["steps"]-1, results)))
--- day20/part2.py
+++ day20/part2.py
@@ -1,5 +1,6 @@
 import os
 
+
 def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
     result = {}
     for key, pos in positions.items():
@@ -7,7 +8,7 @@ def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
             new_fronts = [(x, y)]
             visited = {(x, y): 0}
             directions = {"north": (0, -1), "south": (0, 1),
-                        "west": (-1, 0), "east": (1, 0)}
+                          "west": (-1, 0), "east": (1, 0)}
             steps = 0
             while len(new_fronts) > 0:
                 steps += 1
@@ -19,8 +20,8 @@ def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
                         nx, ny = x + dx, y + dy
                         if (nx, ny) in field and not (nx, ny) in visited:
                             if field[(nx, ny)] == '.':
-                                if (nx,ny) in portals and portals[(nx,ny)]!=key:
-                                    nkey = portals[(nx,ny)]
+                                if (nx, ny) in portals and portals[(nx, ny)] != key:
+                                    nkey = portals[(nx, ny)]
                                     if not key in result:
                                         result[key] = {}
                                     if not nkey in result:
@@ -31,6 +32,7 @@ def calculate_distances(field: dict, portals: dict, positions: dict) -> dict:
                                 new_fronts.append((nx, ny))
     return result
 
+
 dir = os.path.dirname(__file__)
 with open(os.path.join(dir, "input")) as f:
     lines = list(map(list, f.read().rstrip().split("\n")))
@@ -39,11 +41,11 @@ with open(os.path.join(dir, "input")) as f:
     field = {}
     portals = {}
     positions = {}
-    directions = {"north": ((0, -2), (0, -1)), "south": ((0, 1),(0, 2)),
-                  "west": ((-2, 0),(-1, 0)), "east": ((1, 0),(2, 0))}
+    directions = {"north": ((0, -2), (0, -1)), "south": ((0, 1), (0, 2)),
+                  "west": ((-2, 0), (-1, 0)), "east": ((1, 0), (2, 0))}
     for y in range(height):
         for x in range(width):
-            if x<len(lines[y]):
+            if x < len(lines[y]):
                 k = lines[y][x]
                 if k == '.' or k == '#':
                     field[(x, y)] = k
@@ -51,22 +53,22 @@ with open(os.path.join(dir, "input")) as f:
                     for d in directions:
                         dx1, dy1 = directions[d][0]
                         dx2, dy2 = directions[d][1]
-                        c1,c2 = lines[y+dy1][x+dx1],lines[y+dy2][x+dx2]
+                        c1, c2 = lines[y+dy1][x+dx1], lines[y+dy2][x+dx2]
                         if c1.isupper() and c2.isupper():
                             outer = x == 2 or y == 2 or x == width-3 or y == height-3
                             if outer:
                                 label = 'o' + c1 + c2
-                            else: 
+                            else:
                                 label = 'i' + c1 + c2
                             portals[(x, y)] = label
                             if not label in positions:
                                 positions[label] = []
-                            positions[label].append((x,y))
+                            positions[label].append((x, y))
 
     distances = calculate_distances(field, portals, positions)
-    
+
     states = {}
-    new_states = {'0,oAA':{
+    new_states = {'0,oAA': {
         "at": "oAA",
         "level": 0,
         "steps": 0,
@@ -74,7 +76,7 @@ with open(os.path.join(dir, "input")) as f:
     }}
     results = []
     step = 0
-    while len(new_states) > 0 and len(results)<1:
+    while len(new_states) > 0 and len(results) < 1:
         step += 1
         states = new_states
         new_states = {}
@@ -83,9 +85,9 @@ with open(os.path.join(dir, "input")) as f:
             level = state["level"]
             if at == 'iZZ' and level == -1:
                 results.append(state)
-            if at in distances and level>=0:
+            if at in distances and level >= 0:
                 for key, steps in distances[at].items():
-                    if key[0:1]=='o':
+                    if key[0:1] == 'o':
                         new_level = level-1
                         new_at = 'i'+key[1:]
                     else:
@@ -93,8 +95,8 @@ with open(os.path.join(dir, "input")) as f:
                         new_at = 'o'+key[1:]
                     new_path = state["path"] + ',' + key[1:]
                     new_steps = state["steps"] + steps+1
-                    index = ','.join([str(new_level),new_at])
-                    if index in new_states and new_states[index]["steps"]<new_steps:
+                    index = ','.join([str(new_level), new_at])
+                    if index in new_states and new_states[index]["steps"] < new_steps:
                         continue
                     new_states[index] = {
                         "at": new_at,
@@ -103,4 +105,4 @@ with open(os.path.join(dir, "input")) as f:
                         "path": new_path
                     }
 
-    print(min(map(lambda s:s["steps"]-1,results)))
+    print(min(map(lambda s: s["steps"]-1, results)))
