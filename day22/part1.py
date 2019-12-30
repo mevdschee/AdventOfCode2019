806b85b8a611dfc394e8dc7065eaf1404c633a7c
Maurits van der Schee <maurits@vdschee.nl>
2019-12-30 13:06:00 +0100

    formatting

--- day22/part1.py
+++ day22/part1.py
@@ -6,10 +6,10 @@ with open(os.path.join(dir, "input")) as f:
     lines = f.read().strip().split("\n")
     deck = list(range(10007))
     for line in lines:
-        parts = line.rsplit(' ', 1)
+        parts = line.rsplit(" ", 1)
         text = parts[0]
-        if parts[1] == 'stack':
-            parts[1] = '0'
+        if parts[1] == "stack":
+            parts[1] = "0"
         number = int(parts[1])
         new_deck = []
         if text == "deal into new":
@@ -17,9 +17,9 @@ with open(os.path.join(dir, "input")) as f:
         if text == "cut":
             new_deck = deck[number:] + deck[:number]
         if text == "deal with increment":
-            new_deck = [-1]*len(deck)
+            new_deck = [-1] * len(deck)
             for i in range(len(deck)):
-                new_deck[i*number % len(deck)] = deck[i]
+                new_deck[i * number % len(deck)] = deck[i]
         deck = new_deck
 
     print(deck.index(2019))
--- day22/part2.py
+++ day22/part2.py
@@ -8,18 +8,18 @@ with open(os.path.join(dir, "test4")) as f:
     mul = 1
     add = 0
     for line in lines:
-        text, number = line.rsplit(' ', 1)
+        text, number = line.rsplit(" ", 1)
         if text == "deal into new":
             mul *= -1
             add += mul
         if text == "cut":
             add += int(number) * mul
         if text == "deal with increment":
-            mul *= int(number)**(deck-3)
+            mul *= int(number) ** (deck - 3)
         mul %= deck
         add %= deck
     for pos in range(deck):
-        print((pos*mul+add) % deck, end=' ')
+        print((pos * mul + add) % deck, end=" ")
     print()
 
     # Cant solve this.. :-(
