667e10b7a0228261b862d356f0832610d1bfee92
Maurits van der Schee <maurits@vdschee.nl>
2025-12-10 02:31:17 +0100

    part2.py

--- day22/part2.py
+++ day22/part2.py
@@ -1,25 +1,32 @@
 import os
 
-
 dir = os.path.dirname(__file__)
-with open(os.path.join(dir, "test4")) as f:
+with open(os.path.join(dir, "input")) as f:
     lines = f.read().strip().split("\n")
-    deck = 10
-    mul = 1
-    add = 0
-    for line in lines:
+    deck = 119315717514047
+    iterations = 101741582076661
+    position = 2020
+    
+    a = 1
+    b = 0
+    for line in reversed(lines):
         text, number = line.rsplit(" ", 1)
         if text == "deal into new":
-            mul *= -1
-            add += mul
-        if text == "cut":
-            add += int(number) * mul
-        if text == "deal with increment":
-            mul *= int(number) ** (deck - 3)
-        mul %= deck
-        add %= deck
-    for pos in range(deck):
-        print((pos * mul + add) % deck, end=" ")
-    print()
-
-    # Cant solve this.. :-(
+            a = (-a) % deck
+            b = (deck - 1 - b) % deck
+        elif text == "cut":
+            b = (b + int(number)) % deck
+        elif text == "deal with increment":
+            inv = pow(int(number), deck - 2, deck)
+            a = (a * inv) % deck
+            b = (b * inv) % deck
+    
+    final_a = pow(a, iterations, deck)
+    
+    if a == 1:
+        final_b = (b * iterations) % deck
+    else:
+        final_b = (b * (final_a - 1) % deck * pow(a - 1, deck - 2, deck)) % deck
+    
+    result = (final_a * position + final_b) % deck
+    print(result)
